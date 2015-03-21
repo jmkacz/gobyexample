510e37c74ee56bc8c75ce22884980f87c5363975
Jon Kaczynski <jon@birchbox.com>
2015-03-21 15:17:35 -0400

    Adding the full panic output.

--- examples/panic/panic.sh
+++ examples/panic/panic.sh
@@ -6,8 +6,25 @@ panic: a problem
 
 goroutine 1 [running]:
 main.main()
-	/.../panic.go:12 +0x47
-...
+	/.../panic.go:14 +0x64
+
+goroutine 2 [runnable]:
+runtime.forcegchelper()
+	/usr/local/go/src/runtime/proc.go:90
+runtime.goexit()
+	/usr/local/go/src/runtime/asm_amd64.s:2232 +0x1
+
+goroutine 3 [runnable]:
+runtime.bgsweep()
+	/usr/local/go/src/runtime/mgc0.go:82
+runtime.goexit()
+	/usr/local/go/src/runtime/asm_amd64.s:2232 +0x1
+
+goroutine 4 [runnable]:
+runtime.runfinq()
+	/usr/local/go/src/runtime/malloc.go:712
+runtime.goexit()
+	/usr/local/go/src/runtime/asm_amd64.s:2232 +0x1
 exit status 2
 
 # Note that unlike some languages which use exceptions
